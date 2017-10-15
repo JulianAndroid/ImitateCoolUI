2a55b1ae4efe88ffe30d11a00e85ebca9f725aa3
zhuliang <zhuliang@nutstore.net>
2017-10-15 14:57:13 +0800

    完成 FavorView 的布局

--- jikelike/src/main/java/io/julian/imitate/jikelike/widget/FavorView.java
+++ jikelike/src/main/java/io/julian/imitate/jikelike/widget/FavorView.java
@@ -1,8 +1,8 @@
 package io.julian.imitate.jikelike.widget;
 
 import android.content.Context;
-import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
@@ -14,11 +14,13 @@ import io.julian.imitate.jikelike.R;
 
 public class FavorView extends ViewGroup {
 
-    private static final String TAG = "FavorView";
+    private static final float SHINING_SELECTED_DETAL = 1.25F;
+    private static final float SHINING_RING_DETAL = 1.5F;
+    private static final float SHINING_CONTENT_DETAL = 2.25F;
 
     private ImageView mLikeSelected;
-    private ImageView mLikeSelectedShining;
-    private ImageView mLikeUnselected;
+    private ImageView mLikeShining;
+    private View mLikeRing;
 
     public FavorView(Context context) {
         this(context, null);
@@ -35,14 +37,16 @@ public class FavorView extends ViewGroup {
     }
 
     private void createLikeView() {
-        mLikeSelectedShining = new ImageView(getContext());
-        mLikeSelectedShining.setImageResource(R.drawable.ic_messages_like_selected_shining);
-        addView(mLikeSelectedShining);
-
         mLikeSelected = new ImageView(getContext());
         mLikeSelected.setImageResource(R.drawable.ic_messages_like_selected);
         addView(mLikeSelected);
 
+        mLikeShining = new ImageView(getContext());
+        mLikeShining.setImageResource(R.drawable.ic_messages_like_selected_shining);
+        addView(mLikeShining);
+
+        mLikeRing = new View(getContext());
+        addView(mLikeRing);
     }
 
     @Override
@@ -55,73 +59,69 @@ public class FavorView extends ViewGroup {
         final int pright = getPaddingRight();
         final int pbottom = getPaddingBottom();
 
-        int widthSize;
-        int heightSize;
         if (widthMode == MeasureSpec.AT_MOST
                 && heightMode == MeasureSpec.AT_MOST) {
-
             measureChildren(widthMeasureSpec, heightMeasureSpec);
 
-            int selectedWidth = mLikeSelected.getMeasuredWidth();
-            int selectedHeight = mLikeSelected.getMeasuredHeight();
-            int shiningWidth = mLikeSelectedShining.getMeasuredWidth();
-            int shiningHeight = mLikeSelectedShining.getMeasuredHeight();
-            widthSize = Math.max(selectedWidth, shiningWidth) + pleft + pright;
-            heightSize = selectedHeight + shiningHeight / 2 + ptop + pbottom;
+            final int shiningWidth = mLikeShining.getMeasuredWidth();
+            final int shiningHeight = mLikeShining.getMeasuredHeight();
+            final int ringWidth = (int) (shiningWidth * SHINING_RING_DETAL);
+            final int ringMeasureWidth = MeasureSpec.makeMeasureSpec(ringWidth, MeasureSpec.EXACTLY);
+            mLikeRing.measure(ringMeasureWidth, ringMeasureWidth);
+
+            final int widthSize = (int) (shiningWidth * SHINING_CONTENT_DETAL) + pleft + pright;
+            final int heightSize = (int) (shiningHeight * SHINING_CONTENT_DETAL) + ptop + pbottom;
+
+            setMeasuredDimension(widthSize, heightSize);
         } else {
-            widthSize = MeasureSpec.getSize(widthMeasureSpec);
-            heightSize = MeasureSpec.getSize(heightMeasureSpec);
-            final int contentWidth = widthSize - pleft - pright;
-            final int contentHeight = heightSize - ptop - pbottom;
+            final int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+            final int heightSize = MeasureSpec.getSize(heightMeasureSpec);
 
-            BitmapDrawable selectedDrawable = (BitmapDrawable) mLikeSelected.getDrawable();
-            BitmapDrawable shiningDrawable = (BitmapDrawable) mLikeSelectedShining.getDrawable();
-            float selectedWidth = selectedDrawable.getIntrinsicWidth();
-            float selectedHeight = selectedDrawable.getIntrinsicHeight();
-            float shiningWidth = shiningDrawable.getIntrinsicWidth();
-            float shiningHeight = shiningDrawable.getIntrinsicHeight();
+            final int contentWidth = Math.min(widthSize - pleft - pright, heightSize - ptop - pbottom);
+            final int ringWidth = (int) (contentWidth * SHINING_RING_DETAL / SHINING_CONTENT_DETAL);
+            final int selectedWidth = (int) (contentWidth * SHINING_SELECTED_DETAL / SHINING_CONTENT_DETAL);
+            final int shiningWidth = (int) (contentWidth / SHINING_CONTENT_DETAL);
 
-            final int drawableWidth = Math.max(selectedDrawable.getIntrinsicWidth(),
-                    shiningDrawable.getIntrinsicWidth());
-            final int drawableHeight = selectedDrawable.getIntrinsicHeight()
-                    + shiningDrawable.getIntrinsicHeight() / 2;
+            final int ringMeasureSpec, selectedMeasureSpec, shiningMeasureSpec;
+            ringMeasureSpec = MeasureSpec.makeMeasureSpec(ringWidth, MeasureSpec.EXACTLY);
+            selectedMeasureSpec = MeasureSpec.makeMeasureSpec(selectedWidth, MeasureSpec.EXACTLY);
+            shiningMeasureSpec = MeasureSpec.makeMeasureSpec(shiningWidth, MeasureSpec.EXACTLY);
 
-            float desireAspect = Math.min(1.0f * contentWidth / drawableWidth,
-                    1.0f * contentHeight / drawableHeight);
+            mLikeRing.measure(ringMeasureSpec, ringMeasureSpec);
+            mLikeSelected.measure(selectedMeasureSpec, selectedMeasureSpec);
+            mLikeShining.measure(shiningMeasureSpec, shiningMeasureSpec);
 
-            selectedWidth *= desireAspect;
-            selectedHeight *= desireAspect;
-            shiningWidth *= desireAspect;
-            shiningHeight *= desireAspect;
-
-            mLikeSelectedShining.measure(
-                    MeasureSpec.makeMeasureSpec((int) (shiningWidth), MeasureSpec.EXACTLY),
-                    MeasureSpec.makeMeasureSpec((int) (shiningHeight), MeasureSpec.EXACTLY));
-            mLikeSelected.measure(
-                    MeasureSpec.makeMeasureSpec((int) (selectedWidth), MeasureSpec.EXACTLY),
-                    MeasureSpec.makeMeasureSpec((int) (selectedHeight), MeasureSpec.EXACTLY));
+            setMeasuredDimension(widthSize, heightSize);
         }
-        setMeasuredDimension(widthSize, heightSize);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         final int width = getMeasuredWidth();
+        final int height = getMeasuredHeight();
+
         final int childLeft = getPaddingLeft();
         final int childTop = getPaddingTop();
 
-        final int shiningLeft = childLeft / 2 + (width - mLikeSelectedShining.getMeasuredWidth()) / 2;
-        final int shiningTop = childTop;
-        final int shiningWidth = mLikeSelectedShining.getMeasuredWidth();
-        final int shiningHeight = mLikeSelectedShining.getMeasuredHeight();
-        mLikeSelectedShining.layout(shiningLeft, shiningTop, shiningLeft + shiningWidth,
-                shiningTop + shiningHeight);
+        final int ringLeft = childLeft / 2 + (width - mLikeRing.getMeasuredWidth()) / 2;
+        final int ringTop = childTop / 2 + (height - mLikeRing.getMeasuredHeight()) / 2;
+        final int ringWidth = mLikeRing.getMeasuredWidth();
+        final int ringHeight = mLikeRing.getMeasuredHeight();
+        mLikeRing.layout(ringLeft, ringTop, ringLeft + ringWidth, ringTop + ringHeight);
 
-        final int selectLeft = childLeft / 2 + (width - mLikeSelected.getMeasuredWidth()) / 2;
-        final int selectTop = childTop + shiningHeight / 2;
-        final int selectWidth = mLikeSelected.getMeasuredWidth();
-        final int selectHeight = mLikeSelected.getMeasuredHeight();
-        mLikeSelected.layout(selectLeft, selectTop, selectLeft + selectWidth,
-                selectTop + selectHeight);
+        final int selectedWidth = mLikeSelected.getMeasuredWidth();
+        final int selectedHeight = mLikeSelected.getMeasuredHeight();
+        final int selectedLeft = childLeft / 2 + (width - selectedWidth) / 2;
+        final int selectedTop = childTop / 2 + (height - selectedHeight) / 2;
+        mLikeSelected.layout(selectedLeft, selectedTop, selectedLeft + selectedWidth,
+                selectedTop + selectedHeight);
+
+        final int shiningWidth = mLikeShining.getMeasuredWidth();
+        final int shiningHeight = mLikeShining.getMeasuredHeight();
+        final int shiningLeft = childLeft / 2 + (width - shiningWidth) / 2;
+        final int shiningTop = selectedTop - shiningHeight / 2;
+        mLikeShining.layout(shiningLeft, shiningTop, shiningLeft + shiningWidth,
+                shiningTop + shiningHeight);
     }
+
 }
